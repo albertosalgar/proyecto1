5de5ff71cdd2cc517ace9c1c8902aabd4ae3a101
Alberto <alberto.salgar.1@educa.jcyl.es>
2023-04-12 13:43:07 +0200

    commit en EclipseGit1

---
+++ EclipseGit1/src/cambio1/Cambio2.java
@@ -0,0 +1,10 @@
+package cambio1;
+
+public class Cambio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cambio2 desde EclipseGit1");
+	}
+
+}
